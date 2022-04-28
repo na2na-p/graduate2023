756f93314e693b4818bc5a0f1f7cc948252d5b8c
na2na-p <yamamoto.akyshi@gmail.com>
2022-04-28 14:01:38 +0900

    initial commit

---
+++ docs/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import React from 'react';
+
+/**
+ * @param {any} param0 sample param
+ * @return {any} sample return
+ */
+function MyApp({Component, pageProps}: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;

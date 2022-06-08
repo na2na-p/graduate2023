b4442a24942771dec064deca9c5478f6c71c33ec
na2na <na2na@na2na.dev>
2022-06-08 12:48:55 +0900

    リファクタリング

--- vuepress/src/.vuepress/config.ts
+++ vuepress/src/.vuepress/config.ts
@@ -1,46 +1,46 @@
-import { defaultTheme, defineUserConfig } from 'vuepress'
-import { path } from '@vuepress/utils'
-import { registerComponentsPlugin } from '@vuepress/plugin-register-components'
+import {defaultTheme, defineUserConfig} from 'vuepress';
+import {path} from '@vuepress/utils';
+import {registerComponentsPlugin} from '@vuepress/plugin-register-components';
 
 export default defineUserConfig({
-    base: '/',
-    lang: 'ja-JP',
-    title: '就職作品プレゼンテーション',
-    description: '就職作品プレゼンテーション',
-    head: [
-        ['meta', { name: 'robots', content: 'noindex, nofollow, noarchive' }],
+  base: '/',
+  lang: 'ja-JP',
+  title: '就職作品プレゼンテーション',
+  description: '就職作品プレゼンテーション',
+  head: [
+    ['meta', {name: 'robots', content: 'noindex, nofollow, noarchive'}],
+  ],
+
+  // dest: '../docs',
+
+  theme: defaultTheme({
+    navbar: [
+      {
+        text: 'About',
+        link: '/',
+      },
+      {
+        text: 'Works',
+        children: [
+          {text: 'このページについて', link: '/works/aboutThisPage.html'},
+          {text: 'Surveinyance', link: '/works/surveinyance/'},
+          {text: 'Gomamayo-deno', link: '/works/gomamayo-deno/'},
+          {text: 'chmodジェネレータ', link: '/works/chmodGenerator/'},
+          {text: 'フェイストラッカー', link: '/works/faceTracker/'},
+        ],
+      },
+      {
+        text: 'Links',
+        children: [
+          {text: 'GitHub', link: 'https://github.com/na2na-p'},
+          {text: 'vuepressGuide(とりあえず)', link: '/guide'},
+        ],
+      },
     ],
-
-    // dest: '../docs',
-
-    theme: defaultTheme({
-        navbar: [
-            {
-                text: 'About',
-                link: '/',
-            },
-            {
-                text: 'Works',
-                children: [
-                    { text: 'このページについて', link: '/works/aboutThisPage.html' },
-                    { text: 'Surveinyance', link: '/works/surveinyance/' },
-                    { text: 'Gomamayo-deno', link: '/works/gomamayo-deno/' },
-                    { text: 'chmodジェネレータ', link: '/works/chmodGenerator/' },
-                    { text: 'フェイストラッカー', link: '/works/faceTracker/' },
-                ]
-            },
-            {
-                text: 'Links',
-                children: [
-                    { text: 'GitHub', link: 'https://github.com/na2na-p' },
-                    { text: 'vuepressGuide(とりあえず)', link: '/guide' },
-                ]
-            }
-        ]
+  }),
+  plugins: [
+    registerComponentsPlugin({
+      componentsDir: path.resolve(__dirname, './components'),
     }),
-    plugins: [
-        registerComponentsPlugin({
-            componentsDir: path.resolve(__dirname, './components'),
-        }),
-    ],
-})
+  ],
+});
